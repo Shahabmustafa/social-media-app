503dc5c5a386eca776c3ebb0975d1036c2b2e767
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-02-09 18:38:16 +0500

    update

--- lib/view/dashboard/profile/profile_screen.dart
+++ lib/view/dashboard/profile/profile_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:realtime_firebase/res/color.dart';
+import 'package:realtime_firebase/res/component/rounded_button.dart';
 import 'package:realtime_firebase/utls/routes/routes.dart';
 import 'package:realtime_firebase/view_model/profile/profile_contoller.dart';
 import 'package:realtime_firebase/view_model/services/session_manager.dart';
@@ -32,76 +33,82 @@ class _ProfileScreenState extends State<ProfileScreen> {
               builder: (BuildContext context,AsyncSnapshot snapshot){
                 if(snapshot.hasData){
                   Map<String,dynamic> data = snapshot.data!.data() as Map<String,dynamic>;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 100.0,
-                      ),
-                      Stack(
-                        alignment: Alignment.bottomRight,
-                        children: [
-                          Container(
-                            height: 150.0,
-                            width: 150.0,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: AppColors.secondaryTextColor,
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 100.0,
+                        ),
+                        Stack(
+                          alignment: Alignment.bottomRight,
+                          children: [
+                            Container(
+                              height: 150.0,
+                              width: 150.0,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: AppColors.secondaryTextColor,
+                                ),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: data['profile'].toString() == "" ? Image.asset('') : Image(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(data['profile']),
+                                  loadingBuilder: (context,child,loadingProgress){
+                                    if(loadingProgress == null)return child;
+                                    return Center(
+                                      child: CircularProgressIndicator(color: Colors.black,),
+                                    );
+                                  },
+                                ),
                               ),
                             ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(100),
-                              child: data['profile'].toString() == "" ? Image.asset('') : Image(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(data['profile']),
-                                loadingBuilder: (context,child,loadingProgress){
-                                  if(loadingProgress == null)return child;
-                                  return Center(
-                                    child: CircularProgressIndicator(color: Colors.black,),
-                                  );
-                                },
+                            InkWell(
+                              onTap: (){
+                                provider.pickImage(context);
+                              },
+                              child: const CircleAvatar(
+                                backgroundColor: AppColors.primaryTextTextColor,
+                                child: Icon(Icons.add,color: Colors.white,),
                               ),
                             ),
-                          ),
-                          InkWell(
-                            onTap: (){
-                              provider.pickImage(context);
-                            },
-                            child: const CircleAvatar(
-                              backgroundColor: AppColors.primaryTextTextColor,
-                              child: Icon(Icons.add,color: Colors.white,),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      ReusableRow(
-                        iconData: Icons.person,
-                        value: data['userName'],
-                        title: 'User Name',
-                      ),
-                      ReusableRow(
-                        iconData: Icons.email_outlined,
-                        value: data['email'],
-                        title: 'Email',
-                      ),
-                      ReusableRow(
-                        iconData: Icons.phone,
-                        value: data['phone'] == '' ? 'xxxx-xxx-xxx-x' : data['phone'],
-                        title: 'Phone',
-                      ),
-                      InkWell(
-                        onTap: ()async{
-                          await FirebaseAuth.instance.signOut();
-                          Navigator.pushNamed(context, RoutesName.loginScreen);
-                        },
-                        child: Text('Log In'),
-                      ),
-                    ],
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        ReusableRow(
+                          iconData: Icons.person,
+                          value: data['userName'],
+                          title: 'User Name',
+                        ),
+                        ReusableRow(
+                          iconData: Icons.email_outlined,
+                          value: data['email'],
+                          title: 'Email',
+                        ),
+                        ReusableRow(
+                          iconData: Icons.phone,
+                          value: data['phone'] == '' ? 'xxxx-xxx-xxx-x' : data['phone'],
+                          title: 'Phone',
+                        ),
+                        const SizedBox(
+                          height: 40.0,
+                        ),
+                        RoundedButton(
+                            title: 'Log Out',
+                            onTap: ()async{
+                              await FirebaseAuth.instance.signOut();
+                              Navigator.pushNamed(context, RoutesName.loginScreen);
+                            }
+                        ),
+                      ],
+                    ),
                   );
                 }else{
                   return Center(child: CircularProgressIndicator());
--- lib/view_model/Sign Up/signup_controller.dart
+++ lib/view_model/Sign Up/signup_controller.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:realtime_firebase/utls/routes/routes.dart';
 import 'package:realtime_firebase/utls/utls.dart';
